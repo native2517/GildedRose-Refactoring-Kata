6db2ce8c838c3b4086c9e4224645b6a7882f9b22
Courtney Adesile <ca@largeinc.com>
2022-11-22 17:22:29 +0000

    added tests & refactor class

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -16,54 +16,88 @@ final class GildedRose
         $this->items = $items;
     }
 
+    /**
+     * Public function to update quality passed on item name
+     *
+     * @return void
+     */
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
+            $this->updateItemSellin($item);
+            $this->updateItemQuality($item);
+        }
+    }
+
+    /**
+     * Condtional function set item quality value
+     *
+     * @param Item $item
+     * @return Item
+     */
+    private function updateItemQuality(Item $item): Item
+    {
+        /**
+         * @TODO: refactor nested conditional statement, I generally look to rewrite code
+         * if more than 3 nested conditions.
+         */
+        if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->quality > 0) {
+                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                    $item->quality = $item->quality - 1;
                 }
             }
-
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
-
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
+        } else {
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->sell_in < 11) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
                         }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
                     }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+                    if ($item->sell_in < 6) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
+                        }
                     }
                 }
             }
         }
+
+        if ($item->sell_in < 0) {
+            if ($item->name != 'Aged Brie') {
+                if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->quality > 0) {
+                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                            $item->quality = $item->quality - 1;
+                        }
+                    }
+                } else {
+                    $item->quality = $item->quality - $item->quality;
+                }
+            } else {
+                if ($item->quality < 50) {
+                    $item->quality = $item->quality + 1;
+                }
+            }
+        }
+
+        return $item;
+    }
+
+    /**
+     * Condtional function set item sell in value
+     *
+     * @param Item $item
+     * @return Item
+     */
+    private function updateItemSellin(Item $item): Item
+    {
+        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            $item->sell_in = $item->sell_in - 1;
+        }
+
+        return $item;
     }
 }
--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -15,6 +15,52 @@ class GildedRoseTest extends TestCase
         $items = [new Item('foo', 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        $this->assertSame('foo', $items[0]->name);
+    }    
+    
+    
+    /**
+     * Test 'Aged Brie' quality for day 1
+     *
+     * @return void
+     */
+    public function testContentDay1(): void
+    {
+        $items = [new Item('Aged Brie', 2, 0)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $this->assertSame(1, $items[0]->sell_in);
+        $this->assertSame(1, $items[0]->quality);
+    }
+    
+    /**
+     * Test 'Elixir of the Mongoose' quality for day 2
+     *
+     * @return void
+     */
+    public function testContentDay2(): void
+    {
+        $items = [new Item('Elixir of the Mongoose', 4, 6)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $this->assertSame(3, $items[0]->sell_in);
+        $this->assertSame(5, $items[0]->quality);
+    }
+    
+    /**
+     * Test 'Conjured Mana Cake' quality for day 3
+     *
+     * @return void
+     */
+    public function testContentDay3(): void
+    {
+        $items = [new Item('Conjured Mana Cake', 1, 4)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $this->assertSame(0, $items[0]->sell_in);
+        $this->assertSame(3, $items[0]->quality);
     }
 }
